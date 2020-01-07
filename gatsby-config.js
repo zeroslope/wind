0287ded0e2eabb825ca7c5ecb04806b7ef3ede4e
Zeroslope <738543046@qq.com>
2020-01-07 23:55:40 +0800

    chore: 修改默认的参数，添加自定义参数

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `zeroslope's blog`,
+    name: `zeroslope`,
+    siteUrl: `https://zeroslope.github.io`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `👨‍💻‍📝`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/slope_zero`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
+        url: `https://github.com/zeroslope`,
+      }
     ],
   },
   plugins: [
@@ -48,8 +36,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
+        name: `zeroslope's blog`,
+        short_name: `zeroslope`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#fff`,
